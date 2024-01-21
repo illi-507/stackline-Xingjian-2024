c3e6fbb2c05020af9cede5167fd8be4e657d7832
illi-510 <xingjian@adsgency.ai>
2024-01-21 17:13:25 -0500

    Updates

---
+++ precache-manifest.a4f3ccb39608c565eedd8d89ad60cbe7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e48a0dcc000ea0a5a3532ebf5a56f0aa",
+    "url": "/stackline-Xingjian-2024/index.html"
+  },
+  {
+    "revision": "e4409324b0cb570faa68",
+    "url": "/stackline-Xingjian-2024/static/css/main.5a80ef57.chunk.css"
+  },
+  {
+    "revision": "11943fdc391c35319762",
+    "url": "/stackline-Xingjian-2024/static/js/2.edb3a7ba.chunk.js"
+  },
+  {
+    "revision": "7321b725a1bb3b5d8614e6c8674ad461",
+    "url": "/stackline-Xingjian-2024/static/js/2.edb3a7ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c90237dd121fa9c4c1a4",
+    "url": "/stackline-Xingjian-2024/static/js/3.8a416645.chunk.js"
+  },
+  {
+    "revision": "e4409324b0cb570faa68",
+    "url": "/stackline-Xingjian-2024/static/js/main.e333e468.chunk.js"
+  },
+  {
+    "revision": "36805fc918f24af65610",
+    "url": "/stackline-Xingjian-2024/static/js/runtime-main.b9114769.js"
+  },
+  {
+    "revision": "64b21497ca2ad205d903035392064f38",
+    "url": "/stackline-Xingjian-2024/static/media/stackline_logo.64b21497.svg"
+  }
+]);
